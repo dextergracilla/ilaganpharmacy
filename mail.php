9e13d04e674936550e32f630ed779f9b70ace84a
dextergracilla <gracilladt@students.nu-baliwag.edu.ph>
2023-06-02 08:39:36 +0800

    test eight

--- mail.php
+++ mail.php
@@ -1,49 +1,57 @@
 <?php
+
+session_start(); 
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+$email = $_POST["email"];
+$name = $_POST["name"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+ 
+$_SESSION['email'] = $email;
+$_SESSION['name'] = $name;
+$_SESSION['subject'] = $subject;
+$_SESSION['message'] = $message;
+
+
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
-require 'phpmailer/src/SMTP.php';
+//Load Composer's autoloader
+require 'vendor/autoload.php';
 
-if(isset($_POST["send"])){
-    $email = $_POST["email"];
-    $name = $_POST["name"];
-    $subject = $_POST["subject"];
-
-    $msg ='<!DOCTYPE html>
-    <html lang="en"> 
-    <head>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-        <p> Good Day! <strong> '. $name.'! </strong></p>
-        <p>Thank you for your feedback!</p>
-        <br>
-        A medicine a day, keeps the Doctor away!<br>
-        <strong></b>Ilagan Pharmacy</strong>
-
-    </body>
-    </html>';
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
 
-    $mail = new PHPMailer(true);
-    $mail->isSMTP();
-    $mail->Host = "smtp.gmail.com";
-    $mail->SMTPAuth = true;
-    $mail->Username = "dextergracilla@gmail.com";
-    $mail->Password = "uddmpendrwdqqnfu";
-    $mail->SMTPSecure = "tls";
-    $mail->Port = "587";
 
-    $mail->setFrom("dextergracilla@gmail.com","Ilagan Pharmacy");
-    $mail->addAddress($email);
-    $mail->isHTML(true);
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'dextergracilla03@gmail.com';                     //SMTP username
+    $mail->Password   = 'twymzixhexbsajnu';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
+    //Recipients
+    $mail->setFrom('dextergracilla@gmail.com', $name);
+    $mail->addAddress('');     //Add a recipient
+    $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = $subject;
-    $mail->Body = $msg;
+    $mail->Body    = $message;
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
-    header('location:index.html');
+    echo 'Message has been sent';
+
+    header("Location:mail_reply.php");
+    exit();
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
+
 ?>
